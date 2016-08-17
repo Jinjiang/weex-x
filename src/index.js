3c3880f7774be1f7e3ef72ebbc84beeddb06dc31
Jinjiang <zhaojinjiang@me.com>
2016-08-18 02:33:36 +0800

    added source code

---
+++ src/index.js
@@ -0,0 +1,20 @@
+/*!
+ * Vuex By Evan You (http://evanyou.me/)
+ * MIT License
+ * https://github.com/vuejs/vuex/tree/next
+ *
+ * Modified by Jinjiang
+ */
+
+import { Store } from './store'
+import { x } from './mixin'
+import { mapState, mapMutations, mapGetters, mapActions } from './util'
+
+export default {
+  Store,
+  x,
+  mapState,
+  mapMutations,
+  mapGetters,
+  mapActions
+}
